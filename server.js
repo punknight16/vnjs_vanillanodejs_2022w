b8599e2e0ad6293bd4d4b98cfc13065b2b2aeb47
Daniel Miller <daniel@Daniels-MacBook-Pro.local>
2022-08-07 08:24:00 -0700

    created basic index.html, mv old index to 1.html

--- server.js
+++ server.js
@@ -50,5 +50,5 @@ http.createServer(function (request, response) {
         }
     });
 
-}).listen(8125);
-console.log('Server running at http://127.0.0.1:8125/');
+}).listen(8888);
+console.log('Server running at http://127.0.0.1:8888/');
